bdfb01c01aca16e28ab6b98639d0f582dcc7aa37
Yasaka-H <yasaka245.ac@gmail.com>
2019-06-17 23:48:52 +0900

    fixed POST and try to make DELETE

--- app/Kakeibo.rb
+++ app/Kakeibo.rb
@@ -9,16 +9,17 @@ before do
     response.headers['Access-Control-Allow-Origin'] = '*'
   end
 
-post '/post' do
-    # params = JSON.parse request.body.read
-    # date = params[:date]
-    # insertStat = 'insert into list (date, category, item, price, discription) values ( ' + data.to_s + ', "meal", "okashi", 200, "test")'
-    res = db.execute('insert into list (date, category, item, price, discription) values ("2019-06-14", "食費", "okashi", 200, "test")')
-    
-    # p res
-    # db.execute(insertStat)
-    return 'hello'
-    
+post '/post', provides: :json do
+    params = request.body.read
+    hash = Rack::Utils.parse_nested_query(params)
+    date = hash["date"]
+    category = hash["category"]
+    item = hash["item"]
+    price = hash["price"]
+    discription = hash["discription"]
+    res = db.execute("insert into list (date, category, item, price, discription) values ('#{date}', '#{category}', '#{item}', '#{price}', '#{discription}')")
+    return 'Success and please push reload button'
+
 end
 
 
@@ -26,13 +27,13 @@ get '/get_all' do
     i = 0
     body << "["
     db.execute("select * from list") do |row|
-    # body << row.to_json
-        #(i == 0) ? '' : ',' 
-    # p row
     body << (i==0 ? "" : ",") + row.to_json
     i += 1
     end
     body << "]"
-    # body
 end
 
+delete '/delete' do
+    id = params['id']
+    p id
+end
